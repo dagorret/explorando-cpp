29de62735bcff8d987c6915739ef3024e5966340
Carlos Dagorret <dagorret@eco.unrc.edu.ar>
2019-06-27 17:41:54 -0300

    List to 0402

---
+++ list0401.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <ostream>
+
+
+int main()
+{
+    std::cout << "Shape\tSide\n" << "-----\t-----\n";
+    std::cout << "Square\t" << 4 << "\n" <<
+                 "Circle\t?\n";
+}
---
+++ list0403.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <ostream>
+
+
+int main()
+{
+    std::cout << "Shape\t\tSide\n" << "-----\t\t-----\n";
+    std::cout << "Square\t\t" << 4 << "\n";
+    std::cout << "Circle\t\t?\nTriangle\t" << 3 << '\n';
+}
