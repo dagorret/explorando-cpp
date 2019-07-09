4e61e6c6536995b985a1f81517cc2502cbb2428b
Carlos Dagorret <dagorret@gmail.com>
2019-07-09 12:19:27 -0300

    Complete 0806

--- list0806.cpp
+++ list0806.cpp
@@ -8,7 +8,11 @@ int main()
     cout.fill('0');
     cout.width(6);
     cout << 42 << '\n';
-    cout.self(ios_base::left, ios_base::adjustfield);
+    cout.setf(ios_base::left, ios_base::adjustfield);
     cout.width(6);
-    cout << 42 << 
+    cout << 42 << '\n';
+    cout << 42 << '\n';
+    cout.fill('-');
+    cout.width(4);
+    cout << -42 << '\n';
 }
