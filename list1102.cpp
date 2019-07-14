c9797fe66e9a4e45dc65123199f5708b153de468
Carlos Dagorret <dagorret@gmail.com>
2019-07-14 20:53:49 -0300

    Booleanos con números

---
+++ list1102.cpp
@@ -0,0 +1,21 @@
+#include <ios>
+#include <iostream>
+#include <ostream>
+
+int main(int argc, char *argv[])
+{
+  bool b;
+
+  std::cout << std::boolalpha;
+  
+  b = false; std::cout << b << '\n';
+  b = true; std::cout << b << '\n';
+  b = false; std::cout << b << '\n';
+  b = true; std::cout << b << '\n';
+  b = 42; std::cout << b << '\n';
+  b = 3.1415926; std::cout << b << '\n';
+  b = 0; std::cout << b << '\n';
+  b = -0.0; std::cout << b << '\n';
+
+  return 0;
+}
