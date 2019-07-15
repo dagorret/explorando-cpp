6bf430c3c105a26fcbd73753c9efc1c4fcbe5bdb
Carlos Dagorret <dagorret@gmail.com>
2019-07-14 21:12:30 -0300

    No se puede convertir string to bool

---
+++ list1103.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+  std::string empty;
+  bool b;
+
+  b = empty;
+  std::cout << b << '\n';
+  return 0;
+}
