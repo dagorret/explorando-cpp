354432b7a85c668a1a727edaf045abd3222d7c0f
Carlos Dagorret <dagorret@gmail.com>
2019-07-16 01:48:05 -0300

    comentarios

--- list1501.cpp
+++ list1501.cpp
@@ -10,15 +10,19 @@ int main(int argc, char *argv[])
 {
   using namespace std;
 
+  // Definir tipos
   typedef map<string, int> count_map;
   typedef count_map::iterator count_iter;
   typedef string::size_type str_size;
 
+  // Variables
   count_map counts;
   string word;
 
+  // Leer datos de entrada estandar
   while (cin >> word)
     ++counts[word];
+
 
   return 0;
 }
