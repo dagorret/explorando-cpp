40b1200265faa82d9276beccfe62b52482c541b6
Carlos Dagorret <dagorret@gmail.com>
2019-07-16 01:46:27 -0300

    leer datos de la entrada estandar

--- list1501.cpp
+++ list1501.cpp
@@ -14,5 +14,11 @@ int main(int argc, char *argv[])
   typedef count_map::iterator count_iter;
   typedef string::size_type str_size;
 
+  count_map counts;
+  string word;
+
+  while (cin >> word)
+    ++counts[word];
+
   return 0;
 }
