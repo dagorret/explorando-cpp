1e9f624115689917568a78e883ee0352d17f81d3
Carlos Dagorret <dagorret@gmail.com>
2019-07-16 00:49:28 -0300

    map y imprimir con iterator. Add calcular mayor string en map

--- list1401.cpp
+++ list1401.cpp
@@ -3,6 +3,8 @@
 #include <map>
 #include <string>
 #include <ostream>
+#include <iomanip>
+
 
 int main(int argc, char *argv[])
 {
@@ -12,8 +14,16 @@ int main(int argc, char *argv[])
   while (std::cin >> word)
     ++counts[word];
 
-  //Por cada pair de word/counts
+  // Determinar el largo de la palabra más larga
+  std::string::size_type longest(0);
   for (std::map<std::string, int>::iterator iter(counts.begin()); iter != counts.end(); ++iter)
-    std::cout << iter->first << '\t' << iter->second  << "\n";
+	 if (iter->first.size() > longest)
+	   longest = iter->first.size();
+	     
+  //Por cada pair de word/counts
+  const int count_size(10);
+  for (std::map<std::string, int>::iterator iter(counts.begin()); iter != counts.end(); ++iter)
+    std::cout << std::setw(longest)    << std::left  << iter->first
+	      << std::setw(count_size) << std::right << iter->second  << "\n";
   return 0;
 }
