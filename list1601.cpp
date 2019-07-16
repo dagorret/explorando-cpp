037d3f9af970f3b0751f65c9ace835be8c590955
Carlos Dagorret <dagorret@gmail.com>
2019-07-16 02:10:06 -0300

    16 01 - master while

--- list1601.cpp
+++ list1601.cpp
@@ -4,6 +4,20 @@
 
 int main(int argc, char *argv[])
 {
-  
+  int value(0);
+  bool have_value(false);
+  char ch(' ');
+
+  while (std::cin.get(ch)) 
+  {
+    if (ch >= '0' and ch <= '9')
+    {
+      value = ch - '0';
+      have_value = true;
+      while (std::cin.get(ch) and ch >= '0' and ch <= '9')
+	value = value * 10  + ch - '0';
+    }
+
+
   return 0;
 }
