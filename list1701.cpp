7c1edd46f958437d4cd64a1b4dd51bc1f9894a4c
Carlos Dagorret <dagorret@gmail.com>
2019-07-18 00:58:52 -0300

    17 01

--- list1701.cpp
+++ list1701.cpp
@@ -8,5 +8,16 @@
 
 int main()
 {
-  
+  using namespace std;
+
+  typedef map<string, int> count_map;
+  typedef count_map::iterator count_iter;
+  typedef string::size_type str_size;
+
+  count_map counts;
+  string word;
+
+  string okay("ABCDEFGHIJKLMNOPQRSTUWXYZ"
+	      "abcdefghijklmnopqrstuwxyz"
+	      "01234567890-_");
 }
